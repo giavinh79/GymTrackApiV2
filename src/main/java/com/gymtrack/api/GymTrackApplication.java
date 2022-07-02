f0003bcaf5f761a4f69b5485cf09d0e6c14f22a5
GV79 <giavinhlam@gmail.com>
2022-07-02 15:37:45 -0400

    fix CORS

--- src/main/java/com/gymtrack/api/GymTrackApplication.java
+++ src/main/java/com/gymtrack/api/GymTrackApplication.java
@@ -2,13 +2,26 @@ package com.gymtrack.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableTransactionManagement
 public class GymTrackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GymTrackApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GymTrackApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
